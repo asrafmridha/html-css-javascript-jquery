1a118e91d5b525d7feb9c1e24afcede1d126a22f
asraf-mridha <asrafmridha@gmail.com>
2022-07-18 06:38:45 +0600

    fade in && fade out in js

--- app.js
+++ app.js
@@ -14,5 +14,17 @@ jQuery(document).ready(function(){
 
        jQuery(".box").toggle(1000);
     });
+
+    $("#fadein").click(function(){
+        $("#div1").fadeIn();
+        $("#div2").fadeIn("slow");
+        $("#div3").fadeIn(3000);
+      });
+
+      $("#fadeout").click(function(){
+        $("#div1").fadeOut();
+        $("#div2").fadeOut("slow");
+        $("#div3").fadeOut(3000);
+      });
   
   });
